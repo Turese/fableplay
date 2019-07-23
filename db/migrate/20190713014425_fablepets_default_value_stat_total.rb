68b4239bb8ba127144ad85d4b6e26beafb62ebdd
Turese Anderson <turese@berkeley.edu>
2019-07-22 23:17:07 -0700

    fix migrations for heroku

--- db/migrate/20190713014425_fablepets_default_value_stat_total.rb
+++ db/migrate/20190713014425_fablepets_default_value_stat_total.rb
@@ -1,6 +1,6 @@
 class FablepetsDefaultValueStatTotal < ActiveRecord::Migration
   def change
-  	change_column :fablepets, :stat_total, :integer
+  	change_column :fablepets, :stat_total, :integer, 'integer USING CAST(stat_total AS integer)'
   	change_column_default :fablepets, :stat_total, 9
   end
 end
