c90792683e1eed2471d953f1179184317141ede8
Turese Anderson <turese@berkeley.edu>
2019-07-23 13:34:56 -0700

    fix default for changed column for heroku again

--- db/migrate/20190713014425_fablepets_default_value_stat_total.rb
+++ db/migrate/20190713014425_fablepets_default_value_stat_total.rb
@@ -1,5 +1,6 @@
 class FablepetsDefaultValueStatTotal < ActiveRecord::Migration
   def change
+  	change_column_default :fablepets, :stat_total, nil
   	change_column :fablepets, :stat_total, 'integer USING CAST(stat_total AS integer)'
   	change_column_default :fablepets, :stat_total, 9
   end
